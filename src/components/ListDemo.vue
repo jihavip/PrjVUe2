ba2edc0a63238ec5b4394edc30a99539657593ac
Ha <khanhdb@Thanhpvs-MacBook-Pro.local>
2022-04-26 21:05:58 +0700

    add file component ListDemo.vue

---
+++ src/components/ListDemo.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul id="listLang">
+        <li v-for="item in arrListLang">
+            <p>{{ item }}</p>
+        </li>
+    </ul>
+</template>
+<script>
+export default{
+    name: 'listLang',
+    data(){
+        return({
+            arrListLang: ['PHP','JS','Java','C#']
+        })
+    }
+}     
+</script>
+
+<style scoped>
+    p{
+    font-size: 4em;
+    font-weight: 500;
+    margin-bottom: 0.4rem;
+    color:cadetblue;
+    }
+</style>>
